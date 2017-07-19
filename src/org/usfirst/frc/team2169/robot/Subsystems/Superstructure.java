65900b886d1e115bb74071173b77cfd9ed79abec
Unknown <devosmaxwell@gmail.com>
2017-07-18 23:27:45 -0500

    Added empty SuperStructure

---
+++ src/org/usfirst/frc/team2169/robot/Subsystems/Superstructure.java
@@ -0,0 +1,8 @@
+package org.usfirst.frc.team2169.robot.Subsystems;
+
+public class Superstructure {
+
+	//When a task that requires coordination of multiple subsystems is required, this area will be used.
+	//todo figure out of constructor dual-calling will be a problem.  Perhaps convert to init loops if needed.
+	
+}
